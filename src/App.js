38d3657f947518a77bb64fc9c3d624432b8cab0d
Pirouzvn <pirouz.vn@gmail.com>
2022-08-25 17:26:59 +0430

    Co-authored-by: AhmadAlArezi <AhmadAlArezi@users.noreply.github.com> Co-authored-by: mahdi taheri <Mahdi-taherii@users.noreply.github.com> Co-authored-by: Erfan <ErfanMalakouti@users.noreply.github.com> Co-authored-by: Alireza mohammadi <alirezadeve@users.noreply.github.com> Co-authored-by: Ali Ziaei Jazi <aliziaeijazi@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,33 +1,37 @@
 import React from 'react';
 import './App.css';
 import { useSelector, useDispatch } from 'react-redux'
-import { decrement, increment,incrementByAmount } from './redux/slice/calculator'
+import { addNumber, removNumber,getOperator , equal } from './redux/slice/calculator'
 
 function App() {
-  const count = useSelector((state) => state.calculator.value)
+  const calculator = useSelector(state => state.calculator)
+
   const dispatch = useDispatch()
   return (
     <div className="App">
      <div>
-        <button
-          aria-label="Increment value"
-          onClick={() => dispatch(increment())}
-        >
-          Increment
-        </button>
-        <span>{count}</span>
-        <button
-          aria-label="Decrement value"
-          onClick={() => dispatch(decrement())}
-        >
-          Decrement
-        </button>
-        <button
-          aria-label="by amount"
-          onClick={() => dispatch(incrementByAmount(5))}
-        >
-          By Amount
-        </button>
+     <input className="display-box" type="text" id="result" disabled value={
+      calculator.firstNumber + " " + calculator.operator + " " + calculator.SecondNumber + " = " + calculator.result} />
+     <div className='numbers'>
+        <button onClick={()=>dispatch(addNumber(1))}>1</button>
+        <button onClick={()=>dispatch(addNumber(2))}>2</button>
+        <button onClick={()=>dispatch(addNumber(3))}>3</button>
+        <button onClick={()=>dispatch(addNumber(4))}>4</button>
+        <button onClick={()=>dispatch(addNumber(5))}>5</button>
+        <button onClick={()=>dispatch(addNumber(6))}>6</button>
+        <button onClick={()=>dispatch(addNumber(7))}>7</button>
+        <button onClick={()=>dispatch(addNumber(8))}>8</button>
+        <button onClick={()=>dispatch(addNumber(9))}>9</button>
+        <button onClick={()=>dispatch(addNumber(0))}>0</button>
+    </div>
+    <div className='operator'>
+        <button onClick={()=>dispatch(getOperator('*'))}>*</button>
+        <button onClick={()=>dispatch(getOperator('/'))}>/</button>
+        <button onClick={()=>dispatch(getOperator('+'))}>+</button>
+        <button onClick={()=>dispatch(getOperator('-'))}>-</button>
+        <button onClick={()=>dispatch(equal())}> = </button>
+        <button onClick={()=>dispatch(removNumber())}> Back </button>
+    </div>
       </div>
     </div>
   );
--- src/redux/slice/calculator.js
+++ src/redux/slice/calculator.js
@@ -3,30 +3,85 @@ import { createSlice } from "@reduxjs/toolkit";
 const initialState = {
   firstNumber: 0,
   SecondNumber: 0,
-  thirdNumber: 0,
   result: 0,
-  oparator:
+  operator:'',
+  histori :[]
 };
+
+const calculate = {
+  '+': (x,y)=>x+y,
+  '-': (x,y)=>x-y,
+  '/': (x,y)=>x/y,
+  '*': (x,y)=>x*y,
+}
 
 export const calculatorSlice = createSlice({
   name: "calculator",
   initialState,
   reducers: {
-    increment: (state) => {
-      state.value += 1;
+    addNumber: (state,action) => { // 
+      if(state.operator)
+        {
+            state.SecondNumber = state.SecondNumber *10 + action.payload
+        }
+        else
+        {
+            state.firstNumber = state.firstNumber *10 + action.payload
+        }
+        console.log("add , First : " , state.firstNumber)
+        console.log("add , Second : " , state.SecondNumber)
+
     },
-    decrement: (state) => {
-      state.value -= 1;
+    removNumber: (state) => {
+      if(state.operator)
+        {
+            state.SecondNumber = Math.floor(state.SecondNumber / 10)
+        }
+        else
+        {
+            state.firstNumber = Math.floor(state.firstNumber / 10)
+        }
+
+        console.log("rm , First : " , state.firstNumber)
+        console.log("rm , Second : " , state.SecondNumber)
+
+
+
     },
-    incrementByAmount: (state, action) => {
-      state.value += action.payload;
-      console.log(action);
+    getOperator:(state,action)=>{
+      if(!state.operator)
+          state.operator = action.payload
+      else
+        {
+          state.result = calculate[state.operator](state.firstNumber,state.SecondNumber);
+          state.firstNumber = state.result
+          state.operator = action.payload
+          state.SecondNumber=0
+        }
+        console.log("operator" , state.result)
     },
+    equal:(state)=>{
+      if(!state.operator)
+        state.result = state.firstNumber
+      else
+      {
+        
+        state.result = calculate[state.operator](state.firstNumber,state.SecondNumber);
+        console.log("result" , state.result)
+      }
+    }
   },
 });
 
+//fullClear
+//clear active number
+//- + number
+//history
+//pwa
+
+
 // Action creators are generated for each case reducer function
-export const { increment, decrement, incrementByAmount } =
+export const { addNumber, removNumber, getOperator , equal } =
   calculatorSlice.actions;
 
 export default calculatorSlice.reducer;
